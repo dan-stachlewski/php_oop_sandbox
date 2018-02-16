14a38441aec0094bbeda98d8576d9adc94ad18d8
dan-stachlewski <dan.stachlewski@gmail.com>
2018-02-17 10:46:30 +1100

    Exploring Class Instances and the functions get_class() & is_a(,).

---
+++ sandbox/class-instances.php
@@ -0,0 +1,22 @@
+<?php
+
+/**
+ * INSTANCE: a single occurrence of something
+ * INSTANCE OOP: a single OBJECT created from a CLASS DEFINITION
+ * OBJECT & INSTANCE used interchangably
+ * To create an instance we use the 'new': (INSTANTIATE = TO CREATE AN INSTANCE)
+ * $person1 = new Person;
+ * Usually when we create a new instance of an object we assign it to a var at the same time - thus the var is an instance of the object to be used as needed...
+ * $person2 = new Person;
+ * Like a while you were out meme pad...
+ *
+ * get_class($object) - takes an instance as an arg returning the name of the class - pass in instance & get class name
+ * is_a($object,$string) - takes in object & string as args & returns true/false if the object has the same class name as the string
+ *
+ *
+ */
+
+ 
+
+
+?>

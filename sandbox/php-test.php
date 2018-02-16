dc4a81f7feb0c4ad12a31bc367d8db4cec8c70a7
dan-stachlewski <dan.stachlewski@gmail.com>
2018-02-17 10:09:00 +1100

    Created a PHP file to test PHP has been set up correctly, inc the root dir structure.

---
+++ sandbox/php-test.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>HTML5 Template.</title>
+    <link rel="stylesheet" href="css/style.css">
+</head>
+<body>
+
+    <!-- https://www.w3schools.com/php/func_date_date.asp -->
+    <?php echo "Today's date is: " . date("l jS \of F Y h:i:s A"); ?>
+
+</body>
+</html>

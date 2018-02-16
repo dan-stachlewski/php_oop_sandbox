f0265e1346de46c1a8bb71a3c13876474b8107ae
dan-stachlewski <dan.stachlewski@gmail.com>
2018-02-17 10:17:01 +1100

    Created a Class Example File to create classes and learn about get_declared_class & class_exists() functions.

---
+++ sandbox/class-example.php
@@ -0,0 +1,19 @@
+<?php
+
+/**
+ * CLASS BEST PRACTICES:
+ * capitalize & CamelCase class names
+ * Make Nouns Singular
+ * Define each Class in a separate File with the Same Name
+ * Group Class files in a DIR Classes
+ *
+ * get_declared_classes() - PHP keeps track of declared classes, we can use the get_declared_classes() function to return an array of classes PHP is aware of
+ *
+ * class_exists($string) - takes a string as an arg and returns true/false depending on whether the class is in the array of declared classes
+ *
+ */
+
+
+ 
+
+?>

a954a5c31579dac235afa510fbe621275a32612c
dan-stachlewski <dan.stachlewski@gmail.com>
2018-02-17 12:54:10 +1100

    Set up the 1st PHP OOP Challenge set by Kevin (tutor). I have the Requirements clearly written out. Now need to write the Classes, Properties, Methods, Instances etc...

---
+++ sandbox/challenge_01.php
@@ -0,0 +1,22 @@
+<?php
+
+//Define a Class for a Bicycle.
+
+/*  Define the Properties for the Class:
+    var brand;
+    var model;
+    var year;
+    var description;
+    var weight_kg;
+*/
+
+/**
+ * Define the Methods for the Class:
+ * name(): brand, model, year in order of my choosing
+ * weight_lbs(): convert from kg's to lb's where 1kg = 2.2046226218 lbs
+ * set_weight_lbs(): A way to pass in a weight in lbs & have it converted into kg's & store in $weight_kg
+ *
+ * Instantiate a number of different Bicycle Objects
+ * Set & Read Properties
+ * Call all Methods to confirm the work
+ */

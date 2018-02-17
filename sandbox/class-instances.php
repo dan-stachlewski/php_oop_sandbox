c283c4c4cdd9f320e47a644178a915c391e2a21e
dan-stachlewski <dan.stachlewski@gmail.com>
2018-02-17 11:01:22 +1100

    Now that we know how to define a class & create instances of the class we want to flesh out the classes by adding attributes & methods. The functions are really helpful accessing the class name and checking to see if a string has the same name as the class name.

--- sandbox/class-instances.php
+++ sandbox/class-instances.php
@@ -16,7 +16,28 @@
  *
  */
 
- 
+//Defining a class
+ class Student {
 
+ }
+
+//Create  NEW INSTANCES of the class
+$student1 = new Student;
+$student2 = new Student;
+
+//Displays the name of the class
+echo get_class($student1) . '<br /><br />';
+
+//Create an array of Class Names
+$class_names = ['Product','Student','student'];
+//Loop through the array checking to see if the instance name is in the array
+foreach($class_names as $class_name){
+    //The below function will return a True or False if the instance of $student1 is a Class name or not and print that to the browser
+    if(is_a($student1,$class_name)){
+        echo "<span style='text-decoration: underline;'>student1</span> <strong>IS</strong> a {$class_name} <br /><br />";
+    } else{
+        echo "<span style='text-decoration: underline;'>student1</span> <strong>IS NOT</strong> a {$class_name} <br /><br />";
+    }
+}
 
 ?>

8569de92376419e83f9212dcd0d96b5430447622
dan-stachlewski <dan.stachlewski@gmail.com>
2018-02-17 14:26:04 +1100

    Updated the comments in this file.

--- sandbox/class-instances.php
+++ sandbox/class-instances.php
@@ -16,6 +16,40 @@
  *
  */
 
+
+/**
+ * CLASS PROPERTIES: vars to hold object values
+ * Also known as: ATTRIBUTES, CLASS VARIABLES, INSTANCE VARIABLES
+ * Define with 'var' followed by $variable_name
+ * We can set an initial default value
+ *
+ * var $first_name;
+ * var $employed = false;
+ *
+ * To use the properties we need to:
+ * Create an instance of the object:                            $customer = new Person;
+ * Use -> 'arrow notation' to reference/access the property:    $customer->first_name = 'Anna';
+ * -> takes care of $ of var
+ *
+ * = sets avalue
+ * -> reading a value
+ *
+ *
+ * We can return data by:
+ * echo $customer->first_name;
+ * Which Prints: //Anna
+ *
+ * Each of the Instances we create will be different & unique - where OOP gets its power...
+ *
+ * get_class_vars($string) - returns a list of properties defined within  the class - dont need an instace of the class to access these properties, just call name of the class using a string
+ * get_object_vars($object) - does the same thing as get_class_vars however it takes in an instance of an object - they basically return the same info except get_class_vars() will return the default values of the class while get_object_vars() will get the current values of the class
+ * property_exists($mixed,$string) which returns true or false depending on whether the property exists within the class or on the object - $mixed can be a class name as a string or an object instance, the $string is the object property we want to check for
+ *
+ */
+
+
+
+
 //Defining a class
  class Student {
 

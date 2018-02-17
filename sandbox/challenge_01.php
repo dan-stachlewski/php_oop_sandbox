a35e0cb457218b7f630655b145c4483061db4f2f
dan-stachlewski <dan.stachlewski@gmail.com>
2018-02-17 15:25:23 +1100

    I have the code that displays the 1st object instance. I now need to work out how to loop through all 9 instances and display the data from the bicycles.php file.

--- sandbox/challenge_01.php
+++ sandbox/challenge_01.php
@@ -28,6 +28,7 @@ class Bicycle {
     var $year;
     var $description;
     var $weight_kg;
+    var $img_url;
     // var $weight_lbs;
     var $lb_in_kgs = 0.45359237;
     var $kg_in_lbs = 2.2046226218;
@@ -53,14 +54,18 @@ class Bicycle {
 
 }
 
+//{$byc1->img_url}
+
 $byc1 = new Bicycle;
 $byc1->brand = 'Giant Manufacturing Co. Ltd.';
 $byc1->model = 'TCX Cyclocros';
-$byc1->year = 2012;
-$byc1->description = 'Giant Manufacturing Co. Ltd. is a Taiwanese bicycle manufacturer that is the world’s largest bicycle manufacturer. Giant was started started by King Liu in 1972. Giant has manufacturing facilities in Taiwan, the Netherlands, and China.By 2014, Giant had sales in over 50 countries, in over 12,000 retail stores. In 2007, its global sales surpassed 5 million bicycles and US$820 million in global revenue, and by 2012 it had reached 6.3 million bicycles and revenue of US$1.8 billion.';
+$byc1->year = 2017;
+$byc1->img_url = './assets/denali-rb.jpg';
+$byc1->description = "<img  style='float:left;width:170px;height:170px;margin-right:15px;' src='{$byc1->img_url}' alt='' /> Giant Manufacturing Co. Ltd. is a Taiwanese bicycle manufacturer that is the world’s largest bicycle manufacturer. Giant was started started by King Liu in 1972. Giant has manufacturing facilities in Taiwan, the Netherlands, and China.By 2014, Giant had sales in over 50 countries, in over 12,000 retail stores. In 2007, its global sales surpassed 5 million bicycles and US$820 million in global revenue, and by 2012 it had reached 6.3 million bicycles and revenue of US$1.8 billion.";
 $byc1->weight_kg = 6.75;
 $byc1->weight_lbs = 14.8812;
 
 echo $byc1->name() . '<br />';
+echo $byc1->description;
 echo round($byc1->weight_lbs(),2) . '<br />';
 echo round($byc1->set_weight_lbs($byc1->weight_lbs),2) . '<br />';

5a5fd16871b56b952ad97ffeb60aa6aec53f88bc
dan-stachlewski <dan.stachlewski@gmail.com>
2018-02-17 14:25:24 +1100

    Wrote the answer code to the challenge. Need to comment the code still & see how Kevin did it differently.

--- sandbox/challenge_01.php
+++ sandbox/challenge_01.php
@@ -20,3 +20,47 @@
  * Set & Read Properties
  * Call all Methods to confirm the work
  */
+
+class Bicycle {
+
+    var $brand;
+    var $model;
+    var $year;
+    var $description;
+    var $weight_kg;
+    // var $weight_lbs;
+    var $lb_in_kgs = 0.45359237;
+    var $kg_in_lbs = 2.2046226218;
+
+    function name(){
+        return $this->model . ' ' . $this->brand . ' ' . $this->year . '.<br />';
+    }
+
+    function weight_lbs(){
+        $bicycle_in_lbs = ($this->weight_kg/$this->lb_in_kgs);
+        return $bicycle_in_lbs;
+    }
+
+    // function set_weight_lbs($weight_lbs){
+    //     $convert_to_kg = ($this->weight_lbs*$this->lb_in_kgs);
+    //     return $convert_to_kg;
+    // }
+
+    function set_weight_lbs($arg){
+        $convert_to_kg = ($arg*$this->lb_in_kgs);
+        return $convert_to_kg;
+    }
+
+}
+
+$byc1 = new Bicycle;
+$byc1->brand = 'Giant Manufacturing Co. Ltd.';
+$byc1->model = 'TCX Cyclocros';
+$byc1->year = 2012;
+$byc1->description = 'Giant Manufacturing Co. Ltd. is a Taiwanese bicycle manufacturer that is the world’s largest bicycle manufacturer. Giant was started started by King Liu in 1972. Giant has manufacturing facilities in Taiwan, the Netherlands, and China.By 2014, Giant had sales in over 50 countries, in over 12,000 retail stores. In 2007, its global sales surpassed 5 million bicycles and US$820 million in global revenue, and by 2012 it had reached 6.3 million bicycles and revenue of US$1.8 billion.';
+$byc1->weight_kg = 6.75;
+$byc1->weight_lbs = 14.8812;
+
+echo $byc1->name() . '<br />';
+echo round($byc1->weight_lbs(),2) . '<br />';
+echo round($byc1->set_weight_lbs($byc1->weight_lbs),2) . '<br />';

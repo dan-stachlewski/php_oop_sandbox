501cb981236fcf3f4c552f071ab71923d84b057c
dan-stachlewski <dan.stachlewski@gmail.com>
2018-02-19 14:18:13 +1100

    This file holds all the data for the 9 object instances that we were asked to create. I need to add them to the index.php file. I also need to work out a better way to loop through each instance and print out each object instead of copy & pasting the data in 9 times...

---
+++ sandbox/byc-oop/bicycles.php
@@ -0,0 +1,83 @@
+<?php
+
+$byc1 = new Bicycle;
+$byc1->brand = 'GMC';
+$byc1->model = 'Denali Road Bike';
+$byc1->year = 2017;
+$byc1->img_url = '/assets/denali-rb.jpg';
+$byc1->description = "The GMC Denali 700C 21-Speed Road Bike is built around a lightweight aluminum road bike frame. You\'ll stop on a dime with the alloy calipers and brake levers and the high-profile alloy Vitesse racing rims look as good as they perform. Shimano Revo shifters allow you to shift with out taking your hands off the handle bars providing safety and confidence. A Shimano derailleur completes the drive train for quick and smooth gear changes. Lastly, this road bike will help you stay hydrated with the included alloy water bottle cage.";
+$byc1->weight_kg = 6.75;
+$byc1->weight_lbs = 14.8812;
+
+
+$byc2 = new Bicycle;
+$byc2->brand = 'Shimano';
+$byc2->model = 'Goplus Road Bike Commuter Series';
+$byc2->year = 2016;
+$byc2->img_url = '/assets/goplus-rb.jpg';
+$byc2->description = "Our brand newproduct Aluminum Road Bike is universally acknowledged as an excellent commuterexpert striving for speed, practicability and fashion. The bicycle featuring lightweight and sturdy frame can perfectly stand for the test of time and terrible roadsituations, which satisfies you with premium racing performance and dailyriding enjoyment. Easy installation makes the convenient parking and storagecome true. Don’t hesitate to buy it now! It will never fail your zeal forspeed!";
+$byc2->weight_kg = 4.25;
+$byc2->weight_lbs = 9.37;
+
+$byc3 = new Bicycle;
+$byc3->brand = 'Schwinn';
+$byc3->model = 'Volare 1400 Road Bike ';
+$byc3->year = 2016;
+$byc3->img_url = '/assets/schwinn-rb.jpeg';
+$byc3->description = "If speed is the name of your game, then you need a bike that's nimble and agile and built to go fast. Introducing the 700c Men's Volare 1400 by Schwinn, a speedy, light road bike that's sure to quench your need for speed. The light Schwinn aluminum road frame pairs with the steel road fork for agile riding, aided by the lightweight handlebar, stem and seat post componentry. Braking is crisp and shifting is smooth thanks to the Shimano 14-speed integrated shifter/brake lever combo and Shimano derailleurs. Oversized Schwinn road tires keep things fast and smooth, while the alloy double wall rims keep the bike durable and ride enjoyable. Like all Schwinn bikes, the Volare 1400 comes with a limited lifetime warranty for as long as you own the bike. Enjoy the ride on a Schwinn!";
+$byc3->weight_kg = 5.67;
+$byc3->weight_lbs = 12.54;
+
+$byc4 = new Bicycle;
+$byc4->brand = 'Poseidon';
+$byc4->model = 'Triton Road Bike ';
+$byc4->year = 2016;
+$byc4->img_url = '/assets/poseidon-rb.jpg';
+$byc4->description = "Introducing the “TRITON”! This is Poseidon’s flagship road bike in which we’ve spent hours of research and development in manufacturing a bike in order to turn heads, yet still save a few bucks in your pocket. The “TRITON” inherits many traits from its predecessor the “Sport 4.0”. We put the bike back on the drawing board and we listened to our customers! The “TRITON’s” frame geometry provides a more upright position to give the first time roadies a sweet introduction, yet still providing your weekend warrior criterium racer with a light and nimble alloy frame. All that road buzz is dampened from the carbon fiber fork for your long journeys. The “TRITON” is paired with a Shimano Claris 8spd drivetrain that may be ready to rip through your next “KOM”. We didn’t stop there either, we’ve improved our braking system with symmetrical dual pivot brakes for added stopping power down your fast descents.";
+$byc4->weight_kg = 5.2;
+$byc4->weight_lbs = 11.75;
+
+$byc5 = new Bicycle;
+$byc5->brand = 'Shimano';
+$byc5->model = 'Tommaso Fascino Road Bike ';
+$byc5->year = 2018;
+$byc5->img_url = '/assets/tommaso-rb.jpg';
+$byc5->description = "The Tommaso Fascino was designed with one goal in mind, make the best value level road bike available on the market without sacrificing performance. To accomplish this lofty goal, we looked to our bestselling model, the Tommaso Imola, and made a few tweaks to make the ultimate value road bike. Like on the Imola, we didn't cut corners by using aftermarket crankset, non-series derailleurs, or other industry tricks you see the competition do to save a few dollars. For the Fascino, we used a full Shimano Tourney drivetrain to deliver consistent performance throughout your ride.The Fascino offers a huge gearing ratio with 21 powerful speeds, making it the perfect option for a versatile, go anywhere road bike. A full Shimano Tourney drivetrain, gives riders the gives riders smooth shifting that isn't usually expected from a bike at this price level. ";
+$byc5->weight_kg = 4.62;
+$byc5->weight_lbs = 9.25;
+
+$byc6 = new Bicycle;
+$byc6->brand = 'Diamondback';
+$byc6->model = 'Century Sport Road Bike ';
+$byc6->year = 2017;
+$byc6->img_url = '/assets/diamondback-rb.jpg';
+$byc6->description = "We've designed the Century Sport to remove as many barriers as possible for those on the fence about buying a road bike: from fit to ride quality to accessibility. If you're thinking about joining the ranks of road riders, this bike will make your decision easy. We’ve completely redesigned these bikes this year, improving their looks, features, and ride quality. Our 7005 aluminum alloy frameset has formed tubes for durability and great power transfer, making it able to perform when called upon. The overall ride benefits from our Enhanced Performance Geometry, which utilizes a taller headtube to put you in a slightly more upright position than a race bike. Imperceptible to most, the advantages of EPG make themselves apparent towards the end of a ride, when your neck and back experience much less fatigue. The Century Sport has a 2×8 drivetrain and a great mix of FSA and Shimano components to keep you on the road, ensuring that your decision has some staying power.";
+$byc6->weight_kg = 5.9;
+$byc6->weight_lbs = 11.75;
+
+$byc7 = new Bicycle;
+$byc7->brand = 'Giordano Libero';
+$byc7->model = '1.6 Mens Road Bike 2.0';
+$byc7->year = 2017;
+$byc7->img_url = '/assets/giordano-rb.jpg';
+$byc7->description = "Designed for road bike enthusiasts while maintaining value combined with performance, the Giordano Libero 2.0 Road Bike delivers a heck of punch. Built around a lightweight hand crafted 6061 aluminum frame and carbon fork the Libero 2.0 Road Bike features Shimano Sora 18 Speed STI shifters, cranks and derailleurs for miles and miles of reliability. Rotational weight is kept in check with light aluminum 700c wheels. Ranging from 22-23lbs (yes, we weighed it with a kickstand and reflectors) the Giordano Libero 2.0 Road Bike is ready for when you decide to get serious. Available in Men’s Small, Medium and Large.";
+$byc7->weight_kg = 5.9;
+$byc7->weight_lbs = 11.75;
+
+$byc8 = new Bicycle;
+$byc8->brand = 'Criticle Cycles';
+$byc8->model = 'Harper Sindle Speed Commuter Bike';
+$byc8->year = 2016;
+$byc8->img_url = '/assets/criticle-rb.jpg';
+$byc8->description = "This is Critical Cycles brand new fixed-gear/single-speed bike. We want you to choose how you ride, which is why we’ve built this baby around a flip-flop hub. Ride fixed or single-speed with a freewheel. Our bikes come installed on the single-speed side, but you can quickly change the orientation by removing, flipping, and re-installing the rear wheel. These speed demons come equipped with two brakes to ensure your safety, 30mm deep-v rims for a softer ride, and KMC chain for ultimate reliability. Just because we’ve built this bike to be kind to your wallet, we haven’t compromised on the quality of our components or the reliability of our ride.";
+$byc8->weight_kg = 6.33;
+$byc8->weight_lbs = 13.85;
+
+$byc9 = new Bicycle;
+$byc9->brand = 'Pure Fix';
+$byc9->model = 'Fixed Speed Single Speed Fixie Bike';
+$byc9->year = 2018;
+$byc9->img_url = '/assets/pure-fix-rb.jpeg';
+$byc9->description = "Pure Fix Cycles' created the ultimate Original Fixie fixed gear single-speed commuter bike that is perfect for the casual rider looking to get from A to B. This fixie bike starts with the simplicity of a classic urban road frame and straight-leg fork made from durable high tensile steel. The threadless stem holds the riser handlebars equipped with durable rubber grips. Additionally, Pure Fix Cycles uses a classic drome saddle for comfort and Tektro front brakes to ensure a safe, fun ride. 700x28 tyres give you control and comfort while riding on almost any pavement and the three-piece sealed crankset allows for a smooth ride. The gear ratio is 44:15 and the total weight of the bike is approximately 10kg. Designed by Pure Fix Cycles in Los Angeles, California.";
+$byc9->weight_kg = 4.3;
+$byc9->weight_lbs = 9.05;

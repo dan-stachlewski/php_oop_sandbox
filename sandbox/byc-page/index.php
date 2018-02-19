547fc9b6f2fb88bfa5f2d999a1716f393d3153ce
dan-stachlewski <dan.stachlewski@gmail.com>
2018-02-19 14:28:30 +1100

    Added the structure that will display each Bicycle Object Instance in a compact way.

--- sandbox/byc-page/index.php
+++ sandbox/byc-page/index.php
@@ -4,10 +4,21 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>HTML5 Template.</title>
+    <title>Bicycle Class w Object Instances Displayed:</title>
     <link rel="stylesheet" href="css/style.css">
 </head>
 <body>
+    <section class="byc-desc">
+        <h2>Bicycle Make Model & Year.</h2>
+        <article>
+            <img class="img-desc" src="./assets/denali-rb.jpg" width="150" height="150" />
+            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+        </article>
+        <article class="weight-conversion">
+            <div class="desc-lbs"><h3>Weight in lbs.</h3></div>
+            <div class="desc-kgs"><h3>Weight in kgs.</h3></div>
+        </article>
+    </section>
 
 </body>
 </html>

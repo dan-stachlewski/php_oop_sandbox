1e5bf602160dfd23d6df4a972a7097ab098db55e
dan-stachlewski <dan.stachlewski@gmail.com>
2018-02-20 14:47:31 +1100

    Updated the Vehicles Class & Sub-Classes displaying how to override & extend Properties & Methods defined in the Class & overridden & extended in the Sub-Classes.

--- sandbox/inheritance/Vehicle.class.php
+++ sandbox/inheritance/Vehicle.class.php
@@ -10,10 +10,20 @@
 class Vehicle{
 
     //Define Class Attributes
+    var $colours; // added in extend & override classes, sub-classes
+
     var $wheels;
     var $doors;
 
     //Define Class Methods
+    function upload_img($file){
+
+    } // added in extend & override classes, sub-classes
+
+    function is_good_in_rain(){
+        return true;
+    } // added in extend & override classes, sub-classes
+
     function petrol_type(){
         return 'Unleaded';
     }
@@ -26,9 +36,29 @@ class Vehicle{
 
 class Car extends Vehicle{
 
+    //Define Class Attributes
+    var $wheels = 4;  // added in extend & override classes, sub-classes
+    var $doors = 4;  // added in extend & override classes, sub-classes
+    var $convertable = false;  // added in extend & override classes, sub-classes
+
+    //Define Class Methods
+    function is_good_in_rain(){
+        return !$this->convertable = false;
+    }  // added in extend & override classes, sub-classes
+
 }
 
 class Motorcycle extends Vehicle{
+
+    //Define Class Attributes
+    var $wheels = 2;  // added in extend & override classes, sub-classes
+    var $doors = 0;  // added in extend & override classes, sub-classes
+
+    //Define Class Methods
+    function is_good_in_rain(){
+        return false;
+    }  // added in extend & override classes, sub-classes
+
 
 }
 
